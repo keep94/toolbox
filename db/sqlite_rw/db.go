b49d5f299e2a228d5bae959cd9193072e6253637
Travis Keep <keep94@gmail.com>
2019-12-03 12:22:33 -0800

    New db/sqlite_rw package.

---
+++ db/sqlite_rw/db.go
@@ -0,0 +1,248 @@
+// Package sqlite_rw reads and updates sqlite databases using consumers
+// from the github.com/keep94/goconsume package.
+package sqlite_rw
+
+import (
+  "errors"
+  "fmt"
+  "hash/fnv"
+
+  "github.com/keep94/goconsume"
+  "github.com/keep94/gosqlite/sqlite"
+)
+
+const (
+  LastRowIdSQL = "select last_insert_rowid()"
+)
+
+// RowForReading reads a database row into its business object.
+// RowForReading instances can optionally implement EtagSetter if
+// its business object has an etag.
+type RowForReading interface {
+
+  // ValuePtr returns the pointer to this instance's business object.
+  ValuePtr() interface{}
+
+  // Ptrs returns the pointers to be passed to Scan to read the database row.
+  Ptrs() []interface{}
+
+  // Unmarshall updates this instance's business object with the values
+  // stored in the pointers that Ptrs returned.
+  Unmarshall() error
+}
+
+// EtagSetter sets the etag on its business objecct
+type EtagSetter interface {
+
+  // Values returns column values from database with Id column last
+  Values() []interface{}
+
+  // SetEtag sets the etag on this instance's business object
+  SetEtag(etag uint64)
+}
+
+// RowForWriting writes its business object to a database row.
+type RowForWriting interface {
+
+  // Values returns the column values for the database with Id column last.
+  Values() []interface{}
+
+  // Marshall updates the values that Values() returns using this instance's
+  // business object
+  Marshall() error 
+}
+
+// SimpleRow provides empty Marshall / Unmarshall for implementations of
+// RowForReading and RowForWriting
+type SimpleRow struct {
+}
+
+func (s SimpleRow) Marshall() error {
+  return nil
+}
+
+func (s SimpleRow) Unmarshall() error {
+  return nil
+}
+
+// ReadSingle executes sql and reads a single row into row's business object.
+// ReadSingle returns noSuchRow if no rows were found. params provides the
+// values for the question mark (?) place holders in sql.
+func ReadSingle(
+    conn *sqlite.Conn,
+    row RowForReading,
+    noSuchRow error,
+    sql string,
+    params ...interface{}) error {
+  stmt, err := conn.Prepare(sql)
+  if err != nil {
+    return err
+  }
+  defer stmt.Finalize()
+  if err = stmt.Exec(params...); err != nil {
+    return err
+  }
+  return FirstOnly(row, stmt, noSuchRow)
+}
+
+// FirstOnly reads one row from stmt into row's business object. FirstOnly
+// returns noSuchRow if stmt has no rows.
+func FirstOnly(
+    row RowForReading,
+    stmt *sqlite.Stmt,
+    noSuchRow error) error {
+  ptrs := row.Ptrs()
+  if stmt.Next() {
+    if err := readRow(row, stmt, ptrs); err != nil {
+      return err
+    }
+    return nil
+  }
+  return noSuchRow
+}
+
+// ReadRows reads many rows from stmt. For each row read, ReadRows adds
+// row's business object to consumer.
+func ReadRows(
+    row RowForReading,
+    stmt *sqlite.Stmt,
+    consumer goconsume.Consumer) error {
+  ptrs := row.Ptrs()
+  for stmt.Next() && consumer.CanConsume() {
+    if err := readRow(row, stmt, ptrs); err != nil {
+      return err
+    }
+    consumer.Consume(row.ValuePtr())
+  }
+  return nil
+}
+
+// ReadMultiple executes sql and reads multiple rows. Each time a row
+// is read, row's business object is added to consumer. params provides
+// values for question mark (?) place holders in sql.
+func ReadMultiple(
+    conn *sqlite.Conn,
+    row RowForReading,
+    consumer goconsume.Consumer,
+    sql string,
+    params ...interface{}) error {
+  stmt, err := conn.Prepare(sql)
+  if err != nil {
+    return err
+  }
+  defer stmt.Finalize()
+  if err = stmt.Exec(params...); err != nil {
+    return err
+  }
+  return ReadRows(row, stmt, consumer)
+}
+
+// AddRow adds row's business object as a new row in database.
+// The row being added must have auto increment id field. AddRow stores the
+// id of the new row at rowId.
+func AddRow(
+    conn *sqlite.Conn,
+    row RowForWriting,
+    rowId *int64,
+    sql string) error {
+  values, err := InsertValues(row)
+  if err != nil {
+    return err
+  }
+  if err = conn.Exec(sql, values...); err != nil {
+    return err
+  }
+  *rowId, err = LastRowId(conn)
+  return err
+}
+
+// UpdateRow updates a row's business object in the database.
+func UpdateRow(
+    conn *sqlite.Conn,
+    row RowForWriting,
+    sql string) error {
+  values, err := UpdateValues(row)
+  if err != nil {
+    return err
+  }
+  return conn.Exec(sql, values...)
+}
+
+// LastRowId fetches the id of last inserted row.
+func LastRowId(conn *sqlite.Conn) (id int64, err error) {
+  stmt, err := conn.Prepare(LastRowIdSQL)
+  if err != nil {
+    return
+  }
+  defer stmt.Finalize()
+  return LastRowIdFromStmt(stmt)
+}
+
+// LastRowIdFromStmt fetches the last inserted row id. stmt must be
+// created from LastRowIdSQL.
+func LastRowIdFromStmt(stmt *sqlite.Stmt) (id int64, err error) {
+  if err = stmt.Exec(); err != nil {
+    return
+  }
+  if !stmt.Next() {
+    err = errors.New("sqlite_db2: Could not fetch inserted row id")
+    return
+  }
+  err = stmt.Scan(&id)
+  return
+}
+
+// UpdateValues returns the values of the SQL columns to update row
+func UpdateValues(row RowForWriting) (
+    values []interface{}, err error) {
+  if err = row.Marshall(); err != nil {
+    return
+  }
+  return row.Values(), nil
+}
+
+// InsertValues returns the values of the SQL columns to add a new row
+func InsertValues(row RowForWriting) (
+    values []interface{}, err error) {
+  var valuesForUpdate []interface{}
+  if valuesForUpdate, err = UpdateValues(row); err != nil {
+    return
+  }
+  return valuesForUpdate[:len(valuesForUpdate) - 1], nil
+}
+
+func doEtag(row EtagSetter) error {
+  etag, err := computeEtag(row.Values())
+  if err != nil {
+    return err
+  }
+  row.SetEtag(etag)
+  return nil
+}
+
+func computeEtag(values interface{}) (uint64, error) {
+  h := fnv.New64a()
+  s := fmt.Sprintf("%v", values)
+  _, err := h.Write(([]byte)(s))
+  if err != nil {
+    return 0, err
+  }
+  return h.Sum64(), nil
+}
+
+func readRow(
+    row RowForReading, stmt *sqlite.Stmt, ptrs []interface{}) error {
+  if err := stmt.Scan(ptrs...); err != nil {
+    return err
+  }
+  etagSetter, isEtagSetter := row.(EtagSetter)
+  if isEtagSetter {
+    if err := doEtag(etagSetter); err != nil {
+      return err
+    }
+  }
+  if err := row.Unmarshall(); err != nil {
+    return err
+  }
+  return nil
+}
---
+++ db/sqlite_rw/db_test.go
@@ -0,0 +1,209 @@
+package sqlite_rw_test
+
+import (
+  "errors"
+  "github.com/keep94/appcommon/db/sqlite_rw"
+  "github.com/keep94/goconsume"
+  "github.com/keep94/gosqlite/sqlite"
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+var (
+  beginFailed = errors.New("sqlite_db: begin failed.")
+  commitFailed = errors.New("sqlite_db: commit failed.")
+)
+
+func TestDatabase(t *testing.T) {
+  assert := assert.New(t)
+  conn, _ := sqlite.Open(":memory:")
+  createTable(conn)
+  rec1 := Record{Name: "a", Phone: "1"}
+  rec2 := Record{Name: "b", Phone: "2"}
+  rec3 := Record{Name: "a", Phone: "3"}
+  assert.Nil(sqlite_rw.AddRow(
+      conn,
+      (&rawRecord{}).init(&rec1),
+      &rec1.Id,
+      "insert into records (name, phone) values (?, ?)",
+  ))
+  assert.Nil(sqlite_rw.AddRow(
+      conn,
+      (&rawRecord{}).init(&rec2),
+      &rec2.Id,
+      "insert into records (name, phone) values (?, ?)",
+  ))
+  assert.Nil(sqlite_rw.AddRow(
+      conn,
+      (&rawRecord{}).init(&rec3),
+      &rec3.Id,
+      "insert into records (name, phone) values (?, ?)",
+  ))
+  assert.Equal(int64(1), rec1.Id)
+  assert.Equal(int64(2), rec2.Id)
+  assert.Equal(int64(3), rec3.Id)
+  
+  var records []Record
+
+  assert.Nil(sqlite_rw.ReadMultiple(
+      conn,
+      (&rawRecord{}).init(&Record{}),
+      goconsume.AppendTo(&records),
+      "select id, name, phone from records where name = ? order by id asc", "a"))
+
+  assert.Equal(int64(1), records[0].Id)
+  assert.Equal("a", records[0].Name)
+  assert.Equal("1", records[0].Phone)
+  assert.Equal(uint64(0), records[0].Etag)
+  assert.Equal(int64(3), records[1].Id)
+  assert.Equal("a", records[1].Name)
+  assert.Equal("3", records[1].Phone)
+  assert.Equal(uint64(0), records[1].Etag)
+
+  noSuchId := errors.New("No such id")
+
+  var fourthRecord Record
+
+  assert.Equal(sqlite_rw.ReadSingle(
+      conn,
+      (&rawRecordWithEtag{}).init(&fourthRecord),
+      noSuchId,
+      "select id, name, phone from records where id = ?", 4), noSuchId)
+
+  var secondRecord Record
+
+  assert.Nil(sqlite_rw.ReadSingle(
+      conn,
+      (&rawRecordWithEtag{}).init(&secondRecord),
+      noSuchId,
+      "select id, name, phone from records where id = ?", 2))
+  
+  secondEtag := secondRecord.Etag
+
+  secondRecord = Record{}
+
+  assert.Nil(sqlite_rw.ReadSingle(
+      conn,
+      (&rawRecordWithEtag{}).init(&secondRecord),
+      noSuchId,
+      "select id, name, phone from records where id = ?", 2))
+  
+  assert.Equal(int64(2), secondRecord.Id)
+  assert.Equal("b", secondRecord.Name)
+  assert.Equal("2", secondRecord.Phone)
+  assert.Equal(secondEtag, secondRecord.Etag)
+
+  secondRecord.Phone = "1234"
+
+  assert.Nil(sqlite_rw.UpdateRow(
+      conn,
+      (&rawRecord{}).init(&secondRecord),
+      "update records set name = ?, phone = ? where id = ?"))
+
+  secondRecord = Record{}
+
+  assert.Nil(sqlite_rw.ReadSingle(
+      conn,
+      (&rawRecordWithEtag{}).init(&secondRecord),
+      noSuchId,
+      "select id, name, phone from records where id = ?", 2))
+
+  assert.Equal(int64(2), secondRecord.Id)
+  assert.Equal("b", secondRecord.Name)
+  assert.Equal("1234", secondRecord.Phone)
+  assert.NotEqual(secondEtag, secondRecord.Etag)
+
+  assert.NotNil(sqlite_rw.ReadMultiple(
+      conn,
+      (&errorRecord{}).init(&Record{}),
+      goconsume.AppendTo(&records),
+      "select id, name, phone from records"))
+
+  assert.NotNil(sqlite_rw.AddRow(
+      conn,
+      (&errorRecord{}).init(&secondRecord),
+      &secondRecord.Id,
+      "insert into records (name, phone) values (?, ?)"))
+
+  assert.NotNil(sqlite_rw.UpdateRow(
+      conn,
+      (&errorRecord{}).init(&secondRecord),
+      "update records set name = ?, phone = ? where id = ?"))
+}
+
+func createTable(conn *sqlite.Conn) {
+  conn.Exec("create table if not exists records (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)")
+}
+
+type Record struct {
+  Id int64
+  Name string
+  Phone string
+  Etag uint64
+}
+
+type rawRecord struct {
+  sqlite_rw.SimpleRow
+  *Record
+}
+
+func (r *rawRecord) init(bo *Record) *rawRecord {
+  r.Record = bo
+  return r
+}
+
+func (r *rawRecord) Ptrs() []interface{} {
+  return []interface{} {&r.Id, &r.Name, &r.Phone}
+}
+
+func (r *rawRecord) ValuePtr() interface{} {
+  return r.Record
+}
+
+func (r *rawRecord) Values() []interface{} {
+  return []interface{} {r.Name, r.Phone, r.Id}
+}
+
+type rawRecordWithEtag struct {
+  rawRecord
+}
+
+func (r *rawRecordWithEtag) init(bo *Record) *rawRecordWithEtag {
+  r.rawRecord.init(bo)
+  return r
+}
+
+func (r *rawRecordWithEtag) SetEtag(etag uint64) {
+  r.Etag = etag
+}
+
+type errorRecord struct {
+  *Record
+}
+
+func (e *errorRecord) init(bo *Record) *errorRecord {
+  e.Record = bo
+  return e
+}
+
+func (e *errorRecord) Ptrs() []interface{} {
+  return []interface{} {&e.Id, &e.Name, &e.Phone}
+}
+
+func (e *errorRecord) Values() []interface{} {
+  return []interface{} {e.Name, e.Phone, e.Id}
+}
+
+func (e *errorRecord) ValuePtr() interface{} {
+  return e.Record
+}
+
+func (e *errorRecord) Marshall() error {
+  return errors.New("An error happened marshalling")
+}
+
+func (e *errorRecord) Unmarshall() error {
+  return errors.New("An error happened unmarshalling")
+}
+
+
